53abf82565220ee4a8a3700d0126606e400c6d8a
UE63797 <oleg.varaksin@sbb.ch>
2018-06-13 19:15:06 +0200

    More work for thick lines

--- engine/src/core/thickline/thickline.ts
+++ engine/src/core/thickline/thickline.ts
@@ -1,7 +1,7 @@
 import vertexShaderSource from './thickline-vert.glsl';
 import fragmentShaderSource from './thickline-frag.glsl';
 import {createProgram, createShader, resizeCanvasToDisplaySize} from '../webgl-program-service';
-import {mat3} from 'gl-matrix';
+import {mat3, vec2} from 'gl-matrix';
 import {Point} from '../point';
 
 function main() {
@@ -30,14 +30,16 @@ function main() {
 
     const lineWidth = 5;
 
-    // points of trinagles that draw a thick line
-    let pointsT = transformToTriangles(points, lineWidth);
-    const length = pointsT.length;
+    // calculate points of triangles that draw a thick line
+    let tPoints = transformToTrianglePoints(points, lineWidth);
+    const length = tPoints.length;
     let data = new Float32Array(2 * length);
-    for (let pointT of pointsT) {
-        data[data.length] = pointT.x;
-        data[data.length] = pointT.y;
-    }
+    tPoints.forEach((tPoint, index) => {
+        data.set([tPoint.x, tPoint.y], index * 2);
+    });
+
+    console.log(2 * length);
+    console.log(data);
 
     let dataBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, dataBuffer);
@@ -79,54 +81,126 @@ function main() {
     gl.drawArrays(gl.TRIANGLES, 0, length);
 }
 
-function transformToTriangles(points: Point[], lineWidth: number): Point[] {
+function transformToTrianglePoints(points: Point[], lineWidth: number): Point[] {
     let length = points != null ? points.length : -1;
     // we expect at least two points
     if (length <= 1) {
         return [];
     }
 
-    let pointsT: Point[] = [];
-
-
+    // break given line in array of straight lines
     let straightLines: StraightLine[] = [];
     let endIndex = 0;
     while (endIndex < length - 1) {
-        let straightLine = buildStraightLine(endIndex, points);
-        straightLines.push(straightLine);
-        endIndex = straightLine.end[1];
+        let {start, end, endPointIndex} = buildStraightLine(endIndex, points);
+        console.log(`${start.x}, ${start.y}, ${end.x}, ${end.y}, ${endPointIndex}`);
+        straightLines.push({start, end});
+        endIndex = endPointIndex;
     }
 
+    let tPoints: Point[] = [];
+    for (let sl of straightLines) {
+        tPoints.push(...findTrianglePoints(sl, lineWidth));
+    }
 
-    return pointsT;
+    return tPoints;
 }
 
-function buildStraightLine(idx: number, points: Point[]): StraightLine {
+function buildStraightLine(idx: number, points: Point[]): StraightLine & { endPointIndex: number } {
     let point = points[idx];
     let start = {x: point.x, y: point.y};
-    let end = findEndPoint(idx + 1, points);
+    let endPointIndex = findEndPointIndex(start, idx + 1, points);
+    let end = points[endPointIndex];
 
-    return {start, end};
+    return {start, end, endPointIndex};
 }
 
-function findEndPoint(idx: number, points: Point[]): [Point, number] {
+function findEndPointIndex(start: Point, idx: number, points: Point[]): number {
     let end = points[idx];
     let next: Point = null;
     if (idx < points.length - 1) {
-        let next = points[idx + 1];
+        next = points[idx + 1];
     }
 
-    if (next && end.x == next.x || end.y == next.y) {
-        // next and current points are located on a straight line => search on the end point
-        return findEndPoint(idx + 1, points);
+    // compare two lines with points (start, end) and (end, next) if they are parallel
+    if (next && isLinesParallel(start, end, end, next)) {
+        // current and next lines are parallel => search on the end point of the straight line
+        return findEndPointIndex(end, idx + 1, points);
     }
 
-    return [end, idx];
+    return idx;
+}
+
+function isLinesParallel(firstStartPoint: Point, firstEndPoint: Point, secondStartPoint: Point, secondEndPoint: Point): boolean {
+    // calculate and compare slopes
+    let slope1 = 0;
+    let slope2 = 0;
+    if (firstStartPoint.x != firstEndPoint.x) {
+        slope1 = (firstEndPoint.y - firstStartPoint.y) / (firstEndPoint.x - firstStartPoint.x);
+    }
+
+    if (secondStartPoint.x != secondEndPoint.x) {
+        slope2 = (secondEndPoint.y - secondStartPoint.y) / (secondEndPoint.x - secondStartPoint.x);
+    }
+
+    //console.log(`slope1 = ${slope1}, slope2 = ${slope2}`);
+
+    return Math.abs(slope1 - slope2) < 0.00000001;
+}
+
+function findTrianglePoints(straightLine: StraightLine, lineWidth: number): Point[] {
+    // get orthogonal normalized vectors at both line points (start and end)
+    let [vecStart, vecEnd] = getNormalizedOrthogonalVector(straightLine);
+
+    // calculate points for triangles (scale and translate vectors back to the original poisitons)
+    const distance1 = vecStart[0] * lineWidth;
+    const distance2 = vecStart[1] * lineWidth;
+    const distance3 = vecEnd[0] * lineWidth;
+    const distance4 = vecEnd[1] * lineWidth;
+
+    let t1: Point = {x: distance1 + straightLine.start.x, y: distance2 + straightLine.start.y};
+    let t2: Point = {x: -distance1 * lineWidth + straightLine.start.x, y: -distance2 + straightLine.start.y};
+    let t3: Point = {x: distance3 + straightLine.end.x, y: distance4 + straightLine.end.y};
+    let t4: Point = {x: -distance3 + straightLine.end.x, y: -distance4 + straightLine.end.y};
+
+    return [t1, t2, t3, t2, t3, t4];
+}
+
+function getNormalizedOrthogonalVector(straightLine: StraightLine): [vec2, vec2] {
+    let outStart: vec2;
+    let outEnd: vec2;
+
+    if (straightLine.start.x == straightLine.end.x) {
+        outStart = outEnd = vec2.fromValues(0, 1);
+        return [outStart, outEnd];
+    }
+
+    if (straightLine.start.y == straightLine.end.y) {
+        outStart = outEnd = vec2.fromValues(1, 0);
+        return [outStart, outEnd];
+    }
+
+    let spoint: Point = {
+        x: straightLine.start.x - straightLine.end.x,
+        y: straightLine.start.y - straightLine.end.y
+    };
+
+    let epoint: Point = {
+        x: straightLine.end.x - straightLine.start.x,
+        y: straightLine.end.y - straightLine.start.y
+    };
+
+    outStart = vec2.create();
+    outEnd = vec2.create();
+    vec2.normalize(outStart, vec2.fromValues(1, -epoint.x / epoint.y));
+    vec2.normalize(outEnd, vec2.fromValues(1, -spoint.x / spoint.y));
+
+    return [outStart, outEnd];
 }
 
 interface StraightLine {
     start: Point;
-    end: [Point, number]; // end point and index of the end point in the array of original points
+    end: Point;
 }
 
 main();
